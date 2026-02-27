c54d20e9492a20d09a04662d5c7a5a4ea7ab030c
Bill Parker <bill.parker@gmail.com>
2026-02-27 09:37:22 -0600

    added yarn cache

---
+++ tmp_index.ts
@@ -0,0 +1,64 @@
+import { StateGraph, START, END } from "@langchain/langgraph";
+import { OpenCanvasGraphAnnotation } from "./state";
+import { routeNode, cleanState, conditionallyGenerateTitle } from "./router";
+import { routePostWebSearch } from "./web-search-bridge";
+import { registerArtifactFlow } from "./artifact-flow";
+import { customAction } from "./nodes/customAction";
+import { generateArtifact } from "./nodes/generate-artifact";
+import { generateFollowup } from "./nodes/generateFollowup";
+import { generatePath } from "./nodes/generate-path";
+import { reflectNode } from "./nodes/reflect";
+import { updateArtifact } from "./nodes/updateArtifact";
+import { replyToGeneralInput } from "./nodes/replyToGeneralInput";
+import { generateTitleNode } from "./nodes/generateTitle";
+import { updateHighlightedText } from "./nodes/updateHighlightedText";
+import { summarizer } from "./nodes/summarizer";
+import { graph as webSearchGraph } from "../web-search";
+import { rewriteArtifact } from "./nodes/rewrite-artifact";
+import { rewriteArtifactTheme } from "./nodes/rewriteArtifactTheme";
+import { rewriteCodeArtifactTheme } from "./nodes/rewriteCodeArtifactTheme";
+
+const builder = new StateGraph(OpenCanvasGraphAnnotation)
+  .addNode("customAction", customAction)
+  .addNode("generateArtifact", generateArtifact)
+  .addNode("generateFollowup", generateFollowup)
+  .addNode("generatePath", generatePath)
+  .addNode("reflect", reflectNode)
+  .addNode("updateArtifact", updateArtifact)
+  .addNode("replyToGeneralInput", replyToGeneralInput)
+  .addNode("cleanState", cleanState)
+  .addNode("generateTitle", generateTitleNode)
+  .addNode("updateHighlightedText", updateHighlightedText)
+  .addNode("summarizer", summarizer)
+  .addNode("webSearch", webSearchGraph)
+  .addNode("rewriteArtifact", rewriteArtifact)
+  .addNode("rewriteArtifactTheme", rewriteArtifactTheme)
+  .addNode("rewriteCodeArtifactTheme", rewriteCodeArtifactTheme)
+  .addNode("routePostWebSearch", routePostWebSearch)
+  .addEdge(START, "generatePath")
+  .addEdge("generatePath", "replyToGeneralInput")
+  .addConditionalEdges("generatePath", routeNode, [
+    "generateArtifact",
+    "webSearch",
+    "customAction"
+  ])
+  .addEdge("webSearch", "routePostWebSearch")
+  .addEdge("replyToGeneralInput", "cleanState")
+   .addEdge("cleanState", "updateHighlightedText")
+  .addEdge("generateArtifact", "reflect")
+  .addConditionalEdges("cleanState", conditionallyGenerateTitle, [
+    END,
+    "generateTitle",
+    "summarizer",
+  ])
+  .addEdge("generateTitle", END)
+  .addEdge("summarizer", END)
+  .addEdge("generateFollowup", "updateArtifact");
+  .addEdge("summarizer", END)
+  .addEdge("generateFollowup", "updateArtifact");
+
+  .addEdge("cleanState", "rewriteArtifact")
+  .addEdge("cleanState", "rewriteArtifactTheme")
+  .addEdge("cleanState", "rewriteCodeArtifactTheme")
+  .addEdge("cleanState", "customAction")
+  .addEdge("cleanState", "webSearch");
